99707baead3e17490ea9f84018eaee113d5b9999
Abhinav Sinha <114015877+AbhinavSinha02@users.noreply.github.com>
2024-08-16 02:07:46 +0530

    Create 50. POW (x,n) .c

---
+++ 50. POW (x,n) .c
@@ -0,0 +1,10 @@
+double myPow(double x, int n) {
+    if(n==0){
+        return 1.0000;
+    }
+    double half= myPow(x,n/2);
+    if(n%2==0){
+        return half*half;
+    }
+    return (n>0)?half*half*x:half*half/x;
+}
