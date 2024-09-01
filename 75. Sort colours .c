efda577faeae665a2c45d3def45d6397c4b5286e
Abhinav Sinha <114015877+AbhinavSinha02@users.noreply.github.com>
2024-09-02 05:06:51 +0530

    Create 75. Sort colours .c

---
+++ 75. Sort colours .c
@@ -0,0 +1,18 @@
+void sortColors(int* nums, int numsSize) {
+    for(int i=1;i<numsSize;i++){
+        int key= nums[i];
+        int j =i-1;
+        while(j>=0 && nums[j]>key){
+            nums[j+1]=nums[j];
+            j=j-1;
+        }
+        nums[j+1]=key;
+    }
+    printf("[");
+    for(int i=0;i<numsSize;i++){
+        printf("%d",nums[i]);
+        printf(",");
+    }
+    printf("]");
+
+}
